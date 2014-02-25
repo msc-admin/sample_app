9834c86755aa173737337623ed5d45f069a805f3
Mysmilecentral Admin <mysmilecentral@gmail.com>
2014-02-25 14:39:09 -0800

    Finish user edit, update, index, and destroy actions

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,13 +4,13 @@ class User < ActiveRecord::Base
 	before_create :create_remember_token
 	
 	validates :name, presence: true, length: { maximum: 50 }
-	# VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
 	VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z]+)*\.[a-z]+\z/i
   	validates :email, presence: true, 
   					format: 	{ with: VALID_EMAIL_REGEX },
                     uniqueness: { case_sensitive: false }
     has_secure_password
     validates :password, length: { minimum: 6 }
+    validates :password_confirmation, presence: true
     
   def User.new_remember_token
     SecureRandom.urlsafe_base64
